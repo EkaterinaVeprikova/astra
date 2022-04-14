619bf1939617b023330de303f002693ba741a09e
kater.rr <90448920+EkaterinaVeprikova@users.noreply.github.com>
2022-04-15 00:44:37 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,17 +1,16 @@
 CREATE TABLE my_table (
-  id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  name varchar(128) NOT NULL DEFAULT '',
-  email varchar(128) NOT NULL DEFAULT '',
-  year_of_birth int(10) NOT NULL DEFAULT 0,
-  gender int(1) NOT NULL DEFAULT 0,
-  number_of_limbs int(1) NOT NULL DEFAULT 0,
-  biography varchar(128) NOT NULL DEFAULT '',
-  policy int(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (id)
+id INT(10) unsigned NOT NULL AUTO_INCREMENT,
+name VARCHAR(128) NOT NULL DEFAULT '',
+email VARCHAR(30),
+year_of_birth YEAR(4),
+gender VARCHAR(10),
+number_of_limbs INT(5),
+biography TEXT,
+PRIMARY KEY(id)
 );
 
-CREATE TABLE superpowers (
-  id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  name varchar(128) NOT NULL DEFAULT '',
-  PRIMARY KEY (id)
+CREATE TABLE superpowers(
+id INT(10) unsigned NOT NULL AUTO_INCREMENT,
+name VARCHAR(128),
+PRIMARY KEY(id)
 );
